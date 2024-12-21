314d6032526776e3f801ba1de54832ac9a9d6973
fambalaya <fambalaya@msn.com>
2024-12-22 01:42:19 +0800

    U

--- lua/plugins/x.lua
+++ lua/plugins/x.lua
@@ -1,49 +1,49 @@
 return {
 
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "tokyonight-day",
-        },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-day",
     },
-    {
-        -- http://www.baidu.com
-        "fambalaya/url-open",
-        keys = {
-            { "<leader>cb", "<cmd>URLOpenUnderCursor<cr>", desc = "Browse URL under cursor" },
-        },
-        opts = {
-            open_app = function()
-                if jit.os == "Windows" then return [[C:\Program Files (x86)\Google\Chrome\Application\chrome.exe]] end
-            end
-        },
+  },
+  {
+    -- http://www.baidu.com
+    "fambalaya/url-open",
+    keys = {
+      { "<leader>cb", "<cmd>URLOpenUnderCursor<cr>", desc = "Browse URL under cursor" },
     },
-    {
-        "fambalaya/resize-font.nvim",
-        opts = {},
-        keys = { "<C-->", "<C-=>", "<C-ScrollWheelUp>", "<C-ScrollWheelDown>" },
+    opts = {
+      open_app = function()
+        if jit.os == "Windows" then return [[C:\Program Files (x86)\Google\Chrome\Application\chrome.exe]] end
+      end,
     },
-    {
-        "fambalaya/highlight-words.nvim",
-        opts = {},
-        keys = {
-            { "<M-a>", "<cmd>HightlightToggle<cr>" },
-            { "<M-c>", "<cmd>HightlightClear<cr>" },
-        },
+  },
+  {
+    "fambalaya/resize-font.nvim",
+    opts = {},
+    keys = { "<C-->", "<C-=>", "<C-ScrollWheelUp>", "<C-ScrollWheelDown>" },
+  },
+  {
+    "fambalaya/highlight-words.nvim",
+    opts = {},
+    keys = {
+      { "<M-a>", "<cmd>HighlightToggle<cr>" },
+      { "<M-c>", "<cmd>HighlightClear<cr>" },
     },
-    -- games
-    {
-        "alanfortlink/blackjack.nvim",
-        keys = {
-            { "<leader>ab", "<cmd>BlackJackNewGame<cr>", desc = "Black jack" },
-        },
+  },
+  -- games
+  {
+    "alanfortlink/blackjack.nvim",
+    keys = {
+      { "<leader>ab", "<cmd>BlackJackNewGame<cr>", desc = "Black jack" },
     },
-    { "Eandrju/cellular-automaton.nvim" },
-    { "alec-gibson/nvim-tetris" },
-    { "seandewar/nvimesweeper" },
-    { "seandewar/killersheep.nvim" },
-    { "jim-fx/sudoku.nvim" },
+  },
+  { "Eandrju/cellular-automaton.nvim" },
+  { "alec-gibson/nvim-tetris" },
+  { "seandewar/nvimesweeper" },
+  { "seandewar/killersheep.nvim" },
+  { "jim-fx/sudoku.nvim" },
 
-    -- ai
-    { import = "lazyvim.plugins.extras.ai.copilot" },
+  -- ai
+  { import = "lazyvim.plugins.extras.ai.copilot" },
 }
